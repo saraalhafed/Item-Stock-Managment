0c28f2f6ac1f543d64a6c81f090a1d9ee9a03d97
sara <saraalhafiy89@gmail.com>
2024-09-08 22:06:28 +0200

    categories

--- src/components/Modals/ModalInput.jsx
+++ src/components/Modals/ModalInput.jsx
@@ -1,7 +1,20 @@
+import { Field } from 'formik'
 import React from 'react'
 
-export const ModalInput = () => {
+export const ModalInput = ({name,label}) => {
   return (
-    <div>ModalInput</div>
+    <Field
+     type="text"
+     name={name}
+     label={label}
+     variant="outlined"
+  required
+  as={TextField} /*field from formik ,so i want to take mui field style */
+  fullWidth
+  sx={{ mb: 2 }}
+    
+    />
   )
 }
+/* validation proptypes ,both string*/
+export default ModalInput;
